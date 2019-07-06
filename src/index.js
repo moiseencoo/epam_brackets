2c55a8d8386be4a4e3bb03d880b7abae88d23600
Varvara Moiseenko <95varya@mail.ru>
2019-07-06 22:28:56 +0500

    Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // your solution 
+
+  let arr = str.split('');
+  let checkComplete;
+  let arr_index = 0;
+
+  while(checkComplete == undefined) {
+
+    let pair = [
+      arr[arr_index],
+      arr[arr_index + 1]
+    ];
+    
+    if (pair[1] == undefined) {
+      checkComplete = false;
+    }
+
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+
+      if (bracketsConfig[i][0] == pair[0] && bracketsConfig[i][1] == pair[1]) {
+
+         arr.splice(arr_index, 2);
+
+         if(arr.length == 0) {
+           checkComplete = true;
+         } 
+
+        if (arr_index >= 1) {
+          arr_index = arr_index - 2;
+        } else if (arr_index == 0) {
+          arr_index = arr_index - 1;
+        }   
+
+         break;
+      }
+
+    }
+
+    arr_index++;
+
+  }
+
+  return checkComplete;
+
 }
+
